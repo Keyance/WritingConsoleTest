021b2561b72edd138b7a9b23c71b14ad05f017c9
Keyance <klara.konopacova@seznam.cz>
2024-03-07 13:47:01 +0100

    Přidejte soubory projektu.

---
+++ WritingConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using WritingConsoleTest;
+
+Project danmachi = new Project("E:\\Text\\TrackerDemo\\Danmachi.txt", "Danmachi Project");
+
+danmachi.Days.Add(new DateOnly(2024, 3, 1), 1182);
+danmachi.Days.Add(new DateOnly(2024, 3, 2), 974);
+danmachi.Days.Add(new DateOnly(2024, 3, 3), 1211);
+danmachi.Days.Add(new DateOnly(2024, 3, 4), 826);
+danmachi.Days.Add(new DateOnly(2024, 3, 5), 2492);
+danmachi.Days.Add(new DateOnly(2024, 3, 6), 307);
+
+double average = danmachi.GetAverage();
+int sum = danmachi.GetSum();
+
+Console.WriteLine(average + " - " + sum);
---
+++ WritingConsoleTest/Project.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritingConsoleTest
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        string Path;
+        public Dictionary <DateOnly, int> Days = new Dictionary <DateOnly, int> ();
+        public Project(string path, string name)
+        {
+            Name = name;
+            Path = path;
+
+        }
+
+        public double GetAverage ()
+        {
+            double result = 0;
+            int count = 0;
+            int sum = 0;
+
+            foreach (var item in Days)
+            {
+                sum = sum + item.Value;
+                count++;
+            }
+
+            result = sum / count;
+            return result;
+        }
+
+        public int GetSum ()
+        {
+            int sum = 0;
+
+            foreach (var item in Days)
+            {
+                sum = sum + item.Value;
+            }
+
+            return sum;
+        }
+
+
+    }
+}
